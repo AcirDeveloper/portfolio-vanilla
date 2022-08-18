dec6d732fb15b36b8e31dcf12b00543417e09a27
AcirDeveloper <acirdeveloper@gmail.com>
2022-08-18 08:26:26 -0500

    [ADD] tipos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,7 @@
 import { renderPortfolio } from './components/portfolio.js'
-
-let text = ['frontend', 'backend', 'full-stack']
-
-let i = 0
-let j = 1
-let reverse = false
-let interval = setInterval(type, 200)
+import { type } from './components/tipos.js'
 
 document.addEventListener('DOMContentLoaded', () => {
+	type()
 	renderPortfolio()
 })
-
-function type() {
-	let nowText = text[i]
-	document.getElementById('typewriter').innerText = nowText.substr(0, j)
-
-	// reverse direction (erase) if reached end of text
-	if (j == nowText.length + 5) {
-		reverse = true
-	}
-
-	j += reverse ? -2 : 1
-
-	// reset j and go forward (type) if reached beginning
-	if (j <= 0) {
-		j = 1
-		reverse = false
-		i++
-	}
-
-	// reached end of list, start over
-	if (i == text.length) {
-		i = 0
-	}
-}
