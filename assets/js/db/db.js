6decb5432ebed11cf164f9cb405af0f9d05889d0
Acir Developer <acirdeveloper@gmail.com>
2023-05-18 22:11:44 -0500

    Quotes

--- assets/js/db/db.js
+++ assets/js/db/db.js
@@ -1,58 +1,59 @@
 export const items = [
-    {
-        id: 1,
-        title: 'Card',
-        description:
-            'El objetivo es crear una interfaz de usuario atractiva y responsive que muestre información personal básica, como el nombre, la ubicación y las estadísticas de seguidores, así como una imagen de perfil. Este proyecto es ideal para desarrollar habilidades en el diseño y desarrollo frontend',
-        tags: ['HTML', 'CSS', 'JS'],
-        image: './assets/images/card-frontend-mentor.png',
-        repo: '#',
-        web: 'https://my-first-project-card.netlify.app/'
-    },
-    {
-        id: 2,
-        title: 'Ecommerce Academlo',
-        description:
-            'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        tags: ['HTML', 'CSS', 'JS', 'React'],
-        image: './assets/images/ecommerce.png',
-        repo: '#',
-        web: 'https://academlo-jb-jc-ecommerce.netlify.app/'
-    },
-    {
-        id: 3,
-        title: 'Devtter',
-        description: 'Comming soon',
-        tags: ['HTML', 'CSS', 'JS'],
-        image: './assets/images/kiss.jpg',
-        repo: '#',
-        web: '#'
-    },
-    {
-        id: 4,
-        title: 'PokeApi',
-        description: 'Comming soon',
-        tags: ['HTML', 'CSS', 'JS'],
-        image: './assets/images/kiss.jpg',
-        repo: '#',
-        web: '#'
-    },
-    {
-        id: 5,
-        title: 'ClimaTacto',
-        description: 'Comming soon',
-        tags: ['HTML', 'CSS', 'JS'],
-        image: './assets/images/kiss.jpg',
-        repo: '#',
-        web: '#'
-    },
-    {
-        id: 6,
-        title: 'AnimeJVC',
-        description: 'Comming soon',
-        tags: ['HTML', 'CSS', 'JS'],
-        image: './assets/images/kiss.jpg',
-        repo: '#',
-        web: '#'
-    }
+  {
+    id: 1,
+    title: 'Card',
+    description:
+      'El objetivo es crear una interfaz de usuario atractiva y responsive que muestre información personal básica, como el nombre, la ubicación y las estadísticas de seguidores, así como una imagen de perfil. Este proyecto es ideal para desarrollar habilidades en el diseño y desarrollo frontend',
+    tags: ['HTML', 'CSS', 'JS'],
+    image: './assets/images/card-frontend-mentor.png',
+    repo: '#',
+    web: 'https://my-first-project-card.netlify.app/'
+  },
+  {
+    id: 2,
+    title: 'Ecommerce Academlo',
+    description:
+      'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    tags: ['HTML', 'CSS', 'JS', 'React'],
+    image: './assets/images/ecommerce.png',
+    repo: '#',
+    web: 'https://academlo-jb-jc-ecommerce.netlify.app/'
+  },
+  {
+    id: 3,
+    title: 'Citas o Quotes',
+    description:
+      'Esta es una aplicación simple creada con React que muestra citas aleatorias del archivo quotes.json cada vez que se carga la página o se hace clic en el botón "Siguiente cita". También cambia el color del fondo, las letras y el botón de manera aleatoria cada vez que se muestra una nueva cita.',
+    tags: ['HTML', 'CSS', 'JS'],
+    image: './assets/images/quotes.png',
+    repo: 'https://github.com/AcirDeveloper/academlo-react-mono-repo/tree/main/01-et-quotes-app',
+    web: 'https://quotes-app-acirdev.netlify.app/'
+  },
+  {
+    id: 4,
+    title: 'PokeApi',
+    description: 'Comming soon',
+    tags: ['HTML', 'CSS', 'JS'],
+    image: './assets/images/kiss.jpg',
+    repo: '#',
+    web: '#'
+  },
+  {
+    id: 5,
+    title: 'ClimaTacto',
+    description: 'Comming soon',
+    tags: ['HTML', 'CSS', 'JS'],
+    image: './assets/images/kiss.jpg',
+    repo: '#',
+    web: '#'
+  },
+  {
+    id: 6,
+    title: 'AnimeJVC',
+    description: 'Comming soon',
+    tags: ['HTML', 'CSS', 'JS'],
+    image: './assets/images/kiss.jpg',
+    repo: '#',
+    web: '#'
+  }
 ]
